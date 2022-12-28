2db6ca2a7d354e80a713599a449095c5379dad44
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2022-12-28 09:32:16 -0600

    Initial Commit

---
+++ SwiftUI-DragAndDrop/SwiftUI_DragAndDropApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_DragAndDropApp.swift
+//  SwiftUI-DragAndDrop
+//
+//  Created by Mark Volkmann on 12/28/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_DragAndDropApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
